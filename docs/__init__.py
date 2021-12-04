4cc463696a9ed430fd585ea517c7f5033b2ac9c0
xutao <xutao@dustess.com>
2021-12-04 12:44:36 +0800

    新增文档：API自动化平台框架设计

---
+++ docs/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding:utf-8 _*- 
+"""
+@author:TXU
+@file:__init__.py.py
+@time:2021/12/04
+@email:tao.xu2008@outlook.com
+@description:
+"""
+
+if __name__ == '__main__':
+    pass
